6a2cde2c8b87f79bc90743a383930d66682e4a3b
Yin <doyin@deloitte.com.cn>
2024-03-27 17:45:46 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const getArraySum = require('./getArraySum')
+const truncateString = require('./truncateString')
+const isPalindrome = require('./isPalindrome')
+const getMaxValue = require('./getMaxValue')
+const formatDate = require('./formatDate')
+
+module.exports = (num) => {
+    return num % 2 !== 0;
+  }
